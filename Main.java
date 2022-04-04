b718ac5a14d34dbc0cad42e33ed9091d52372af3
VitalyRezanov <getahur.rezanov@gmail.com>
2022-04-04 17:23:35 +0300

    Add Part One

---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.netcracker;
+
+import com.netcracker.tasksTwo.PartOne;
+
+public class Main {
+    public static void main(String[] args) {
+
+        PartOne.magicBox();
+
+        System.out.println(PartOne.getDegrees(-2456));
+
+        System.out.println(PartOne.getFactorial(1000));
+
+        PartOne.lottery ();
+
+        PartOne.getNonEmptyString(" Hi   jon ");
+		
+    }
+}
+
